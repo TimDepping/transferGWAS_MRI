1d5c5acca68118bc2010b72dc253dbcd2f63ca36
Laurin Siefermann <siefermann.laurin@gmail.com>
2023-01-09 11:19:15 +0100

    added normalization feature

--- input/scripts/create_classification_csv.py
+++ input/scripts/create_classification_csv.py
@@ -6,13 +6,27 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('img_dir', type=str, help='path to input images (png)')
     parser.add_argument('--out_dir', type=str, default="/dhc/groups/mpws2022cl1/input/", help='Directory to save the csv file')
+    parser.add_argument('--normalize_label', dest="normalize_label", action="store_true", default=False, help="normalize input labels")
     args = parser.parse_args()
 
     import_data = pd.read_csv("/dhc/groups/mpws2022cl1/input/cardio_44k.csv")
 
-    df = import_data.loc[:, ['eid', '22420-2.0']].rename(columns={'eid': 'image', '22420-2.0': 'Ejection Fraction'})
-    df = df.dropna(subset=['Ejection Fraction'])
+    # '22425-2.0': 'Cardiac Index'
+    # '22420-2.0': 'Ejection Fraction'
+    label = 'Ejection Fraction'
+    label_code = '22420-2.0'
 
+    df = import_data.loc[:, ['eid', label_code]].rename(columns={'eid': 'image', label_code: label})
+    df = df.dropna(subset=[label])
+
+    if (args.normalize_label):
+        # Calculate the mean and standard deviation of the Cardiac index / Ejection Fraction column
+        mean_value = df[label].mean()
+        std_value = df[label].std()
+        
+        # Normalize the Cardiac index column
+        df[label] = df[label].apply(lambda x: (x - mean_value) / std_value)
+    
     df['image'] = df['image'].astype(str)
     df['image'] = df['image'].apply(lambda x: str(x) + '_CINE_segmented_LAX_4Ch_RGB_0-16-39')
 
@@ -28,8 +42,15 @@ def main():
     # Use the boolean mask to filter the DataFrame and remove the rows that are not in the list of filenames
     df = df[mask]
 
+    ## create filename string from label 
+    filename_label = label.replace(" ", "")
+    filename_label = filename_label[0].lower() + filename_label[1:]
+    filename_label += "_"
+    if (args.normalize_label):
+        filename_label += "normalized_"
+
     available_labels = len(df)
-    filename = 'ejectionFraction_' + str(available_labels) + '.csv'
+    filename = filename_label + str(available_labels) + '.csv'
     filepath = os.path.join(args.out_dir, filename)
 
     df.to_csv(filepath, index=False)
