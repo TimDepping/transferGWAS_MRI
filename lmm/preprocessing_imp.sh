3b5ca1ab1b6aff75a0558a5f9e2343d6b01258f2
Tim Depping <tim-lorenz@depping.org>
2023-01-12 16:26:33 +0100

    adapt preprocessing for imputed genetic data

--- lmm/preprocessing_imp.sh
+++ lmm/preprocessing_imp.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# TODO fill in the path to your plink2 install, if not in global path
-# alias plink2=/path/to/plink2
+alias plink2=/dhc/projects/mpws2022cl1/plink2
 
-# TODO fill in space-separated file with columns FID and IID, no header
-INDIV=/path/to/indiv.txt
+OUTPUT_DIR=$1
+INDIV=$2
+# number of threads to use
+THREADS=$3
+
+TMP_DIR=$OUTPUT_DIR/.tmp
 
 # Hardy-Weinberg p-value (1e-12)
 HWE_PVAL=0.000000000001
@@ -12,16 +15,15 @@ HWE_PVAL=0.000000000001
 # minor allele frequency
 MAF=0.001
 
-# number of threads to use
-THREADS=40
-
-
+rm -r ${TMP_DIR}
+rm -r ${OUTPUT_DIR}
+mkdir -p ${TMP_DIR}
+mkdir -p ${OUTPUT_DIR}
 
 for i in {1..22}; do
-	# INPUT=${IMP_FN}${i}${IMP_SUFF}
-	INPUT="/path/to/imputed/chr${i}.bgen"
-	SAMPLE="/path/to/imputed/chr${i}.sample"
-	OUTPUT="/path/to/output/chr${i}"
+	INPUT="/dhc/projects/ukbiobank/original/genetics/imputed/EGAD00010001474/ukb_imp_chr${i}_v3.bgen"
+	SAMPLE="/dhc/projects/ukbiobank/original/genetics/imputed/sample/ukb40502_imp_chr${i}_v3_s487296.sample"
+	OUTPUT="${OUTPUT_DIR}/chr${i}"
 
 	plink2 --bgen ${INPUT} 'ref-first' --sample ${SAMPLE} --keep ${INDIV} --maf ${MAF} --hwe ${HWE_PVAL} --threads ${THREADS} --export bgen-1.2 bits=8 --out ${OUTPUT}
 
