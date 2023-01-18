ce2a4592b710867319380995ffb54387796d9dc1
Tim Depping <tim-lorenz@depping.org>
2023-01-18 16:16:32 +0100

    drop duplicate IIDs in pca extreme values script export duplicate images with _dup suffix

--- pca_extreme_values.py
+++ pca_extreme_values.py
@@ -3,11 +3,14 @@ import argparse
 import os
 import shutil
 
-def copy_image(iid, pc_value, source_dir, output_dir):
+def copy_image(iid, source_dir, output_dir):
     files_with_iid = [filename for filename in os.listdir(source_dir) if filename.startswith(str(iid))]
     if len(files_with_iid) == 1:
         first_filename = files_with_iid[0]
-        shutil.copyfile(f'{source_dir}/{first_filename}', f'{output_dir}/{pc_value}_{iid}.png')
+        output_file_path = f'{output_dir}/{iid}.png'
+        if os.path.isfile(output_file_path):
+            output_file_path = f'{output_dir}/{iid}_dup.png'
+        shutil.copyfile(f'{source_dir}/{first_filename}', output_file_path)
     elif len(files_with_iid) > 1:
         print("There exists multiple files for this iid.")
     else:
@@ -24,15 +27,15 @@ def main():
     os.makedirs(args.output_dir, exist_ok=True)
 
     df = pd.read_csv(args.input, delim_whitespace=True)
-
+    df.drop_duplicates(subset='IID', keep='last', inplace=True)
     pc_columns = [column for column in df if column.startswith('PC')]
 
     extreme_values_df = pd.DataFrame(columns=['IID','PC','PC_VALUE'])
     for pc_column in pc_columns:
         pc_df = df[['IID', pc_column]]
-        df.sort_values(by= pc_column, ascending=True, inplace=True)
-        pc_min= pc_df.nsmallest(args.n_extreme+1, columns=pc_column)
-        pc_max= pc_df.nlargest(args.n_extreme+1, columns=pc_column)
+        pc_df = pc_df.sort_values(by= pc_column, ascending=True)
+        pc_min = pc_df.nsmallest(args.n_extreme, columns=pc_column)
+        pc_max = pc_df.nlargest(args.n_extreme, columns=pc_column)
         pc_extreme = pd.concat([pc_min, pc_max])
         pc_output_dir=f'{args.output_dir}/{pc_column}'
         os.makedirs(pc_output_dir, exist_ok=True)
@@ -41,7 +44,6 @@ def main():
             pc_value=str(row[pc_column])
             extreme_values_df.loc[len(extreme_values_df)]=[iid, pc_column, pc_value] 
             copy_image(iid=iid,
-            pc_value=pc_value,
             source_dir="/dhc/groups/mpws2022cl1/images/heart/png/50000_RGB_0-16-39",
             output_dir=pc_output_dir)
     extreme_values_df.to_csv(f'{args.output_dir}/extreme_ids.csv', index=False)
