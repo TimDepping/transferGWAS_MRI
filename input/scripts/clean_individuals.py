70680e75bd2c7ef755ca5ec7572fa9f63464e55e
Tim Depping <tim-lorenz@depping.org>
2023-01-18 16:22:15 +0100

    drop duplicates in clean_individuals script change output dir to path in clean_individuals script

--- input/scripts/clean_individuals.py
+++ input/scripts/clean_individuals.py
@@ -4,34 +4,29 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('indiv_csv', type=str, help='path to csv file holding IID & path to img')
-    parser.add_argument('exclude_csv', type=str, help='path to csv file holding IIDs to exclude')
-    parser.add_argument('--out_dir', type=str, default="/dhc/groups/mpws2022cl1/images/heart/png/", help='Directory to save the cleaned csv file')
+    parser.add_argument('indiv_csv', type=str, help='Path to csv file holding IID and the path to image e.g. 1000_RGB_0-16-39.csv')
+    parser.add_argument('exclude_csv', type=str, help='Path to csv file holding IIDs to exclude')
+    parser.add_argument('output_csv', type=str, default="./out/cleaned_indiv.csv", help='Path to save the cleaned csv file')
     args = parser.parse_args()
 
     # Read in the IIDs to exclude
-    to_exclude = pd.read_csv(args.exclude_csv, sep=";")['IID'].tolist()
-
+    IIDs_to_exclude = pd.read_csv(args.exclude_csv)['IID'].tolist()
     # Read the main file and filter out rows that contain an IID in the set of IIDs to exclude
-    indiv = pd.read_csv(args.indiv_csv)
-    indiv_cleaned = indiv[~indiv['IID'].isin(to_exclude)]
+    indiv = pd.read_csv(args.indiv_csv, sep=",")
+    indiv_cleaned = indiv[~indiv['IID'].isin(IIDs_to_exclude)]
+
+    # Drop IIDs which appear multiple times and only keep the last occurence
+    indiv_cleaned = indiv_cleaned.drop_duplicates(subset=indiv.columns[0],keep='last')
 
     number_IIDs_indiv = len(indiv)
     number_IIDs_indiv_cleaned = len(indiv_cleaned)
     removed = number_IIDs_indiv - number_IIDs_indiv_cleaned
     print("Removed IIDs: " + str(removed))
 
-    cleaned_csv_name = get_filename(args.indiv_csv)
-    cleaned_csv_filepath = os.path.join(args.out_dir, cleaned_csv_name)
-
     # Write the filtered data to a new CSV file
-    indiv_cleaned.to_csv(cleaned_csv_filepath, index=False)
+    indiv_cleaned.to_csv(args.output_csv, index=False)
 
-    print("Successfully created: " + cleaned_csv_filepath)
-
-def get_filename(file_path):
-    base_name = os.path.basename(file_path)
-    return base_name.split('.')[0] + '_cleaned.csv'
+    print("Successfully created: " + args.output_csv)
 
 if __name__ == '__main__':
     main()
