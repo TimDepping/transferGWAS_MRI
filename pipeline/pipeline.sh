2e83fd3cc67f01903c6b813d6ce36728fb6bd506
Tim Depping <tim-lorenz@depping.org>
2023-01-23 15:52:42 +0100

    - log to one file - use most available resource

--- pipeline/pipeline.sh
+++ pipeline/pipeline.sh
@@ -4,8 +4,8 @@ date
 hostname -f
 
 # Parameters
-IDENTIFIER=50000_RGB_0-16-39
-N_PCS=10
+IDENTIFIER=1000_RGB_0-16-39
+N_PCS=1
 MODEL=resnet50
 LAYER=L4
 
@@ -13,7 +13,7 @@ LAYER=L4
 IMAGE_DIR=/dhc/groups/mpws2022cl1/images/heart/png/$IDENTIFIER
 INPUT_INDIV_FILE=/dhc/groups/mpws2022cl1/images/heart/png/$IDENTIFIER.csv
 EXCLUDED_INDIV_FILE=/dhc/groups/mpws2022cl1/images/heart/png/excluded_indiv.csv
-
+LOG_FILE=job.log
 
 # If `imagenet`: load the default pytorch weights
 # else: specify path to `.pt` with state dict
@@ -28,18 +28,19 @@ BASE_NAME=$(basename ${PRETRAINING})
 MODEL_NAME=${BASE_NAME%.*}
 fi
 
-# OUTPUT_DIR=./output/${IDENTIFIER}_${MODEL_NAME}
-OUTPUT_DIR=/dhc/groups/mpws2022cl1/output/${IDENTIFIER}_${MODEL_NAME}
+OUTPUT_DIR=./output/${IDENTIFIER}_${MODEL_NAME}
+# OUTPUT_DIR=/dhc/groups/mpws2022cl1/output/${IDENTIFIER}_${MODEL_NAME}
+mkdir -p $OUTPUT_DIR
 
 # Feature condensation (Create embeddings)
-feature_condensation=$(sbatch --parsable pipeline/feature_condensation.slurm $INPUT_INDIV_FILE $EXCLUDED_INDIV_FILE $OUTPUT_DIR $IMAGE_DIR $N_PCS $MODEL $PRETRAINING $LAYER)
+feature_condensation=$(sbatch --parsable --output=$OUTPUT_DIR/$LOG_FILE pipeline/feature_condensation.slurm $INPUT_INDIV_FILE $EXCLUDED_INDIV_FILE $OUTPUT_DIR $IMAGE_DIR $N_PCS $MODEL $PRETRAINING $LAYER)
 
 # Run LMM
 EMBEDDINGS_FILE=${OUTPUT_DIR}/${MODEL}_${MODEL_NAME}_$LAYER.txt
-lmm=$(sbatch --parsable --dependency=afterok:$feature_condensation pipeline/lmm.slurm $OUTPUT_DIR $EMBEDDINGS_FILE $N_PCS)
+lmm=$(sbatch --parsable --dependency=afterok:$feature_condensation --output=$OUTPUT_DIR/$LOG_FILE pipeline/lmm.slurm $OUTPUT_DIR $EMBEDDINGS_FILE $N_PCS)
 
 # Plot and change file permissions of output dir
-plot=$(sbatch --dependency=afterok:$lmm pipeline/plot.slurm $OUTPUT_DIR)
+plot=$(sbatch --dependency=afterok:$lmm --output=$OUTPUT_DIR/$LOG_FILE pipeline/plot.slurm $OUTPUT_DIR)
 
 # show dependencies in squeue output:
 squeue -u $USER -o "%.8A %.4C %.10m %.20E"
