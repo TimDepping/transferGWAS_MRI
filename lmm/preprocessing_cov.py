0cc2538466cee7821dea349d72708ecb4bed950a
Tim Depping <tim-lorenz@depping.org>
2023-01-23 15:20:38 +0100

    add covariates to script

--- lmm/preprocessing_cov.py
+++ lmm/preprocessing_cov.py
@@ -2,32 +2,39 @@ import argparse
 import numpy as np
 import pandas as pd
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('input', type=str, help='Input UKB covariate file')
-    parser.add_argument('--indiv', type=str, help='Path to subset of individuals')
-    parser.add_argument('--add_cov', nargs=2, action='append', help='Additional covariates as key-value pairs (as in `--add_cov 123-0.0 mytrait`)')
-    parser.add_argument('--output', type=str, default='covariates.txt', help='Output filename')
+    parser.add_argument('--indiv', type=str,
+                        help='Path to subset of individuals')
+    parser.add_argument('--add_cov', nargs=2, action='append',
+                        help='Additional covariates as key-value pairs (as in `--add_cov 123-0.0 mytrait`)')
+    parser.add_argument('--output', type=str,
+                        default='covariates.txt', help='Output filename')
     args = parser.parse_args()
 
     columns = {
-            '31-0.0':       'sex',                  # sex
-            # '21022-0.0':    'age',                  # age at recruitment
-            # '54-0.0':       'assessment_center',    # assessment center
-            # '22000-0.0':    'geno_batch',           # genotyping batch
-            # '20116-0.0',    # smoking status, self-reported touchscreen
-            # '21000-0.0',    # ethnic background, self-reported touchscreen
-            }
+        '31-0.0':   'sex',  # sex
+        '1249-0.0': 'past_tobacco',  # past tobacco smoking
+        '54-0.0':   'assessment_center',    # assessment center
+        '3079-0.0': 'pace_maker',  # pace-maker
+        '22000-0.0':    'geno_batch',  # genotyping batch
+        '22426-0.0':    'heart_rate',   # average heart rate
+        '21022-0.0':    'age',  # age at recruitment
+        '21001-0.0':    'bmi',  # bmi
+    }
 
-    # for i in range(1, 41):
-    #     columns[f'22009-0.{i}'] = f'genet_PC_{i}'
+    for i in range(1, 41):
+        columns[f'22009-0.{i}'] = f'genet_PC_{i}'
 
     if args.add_cov:
         for key, val in args.add_cov:
             columns[key] = val
 
     print('reading covariate file...')
-    df = pd.read_csv(args.input, index_col=0, usecols=['eid']+list(columns.keys()))
+    df = pd.read_csv(args.input, index_col=0, usecols=[
+                     'eid']+list(columns.keys()))
     # df = pd.read_csv(args.input, nrows=10, index_col=0, usecols=['eid']+list(columns.keys()))
     print('preparing data...')
     df.rename(columns, axis=1, inplace=True)
@@ -46,9 +53,8 @@ def main():
     cols = cols[-2:] + cols[:-2]
     df = df[cols]
 
-
     df.to_csv(args.output, sep=' ', index=False)
-    
-    
+
+
 if __name__ == '__main__':
     main()
--- lmm/run_lmm.py
+++ lmm/run_lmm.py
@@ -21,91 +21,103 @@ import toml
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, help='Parse arguments from config file')
-    parser.add_argument('--bed', type=str, help='path to microarray bed file (multiple files e.g. via `foo_{1:22}.bed`)')
-    parser.add_argument('--bim', type=str, help='path to microarray bim file (multiple files e.g. via `foo_{1:22}.bim`)')
+    parser.add_argument('--config', type=str,
+                        help='Parse arguments from config file')
+    parser.add_argument(
+        '--bed', type=str, help='path to microarray bed file (multiple files e.g. via `foo_{1:22}.bed`)')
+    parser.add_argument(
+        '--bim', type=str, help='path to microarray bim file (multiple files e.g. via `foo_{1:22}.bim`)')
     parser.add_argument('--fam', type=str, help='path to microarray fam file')
-    parser.add_argument('--cov', type=str, help='path to space-separated covariate file; first two columns should be FID and IID')
+    parser.add_argument(
+        '--cov', type=str, help='path to space-separated covariate file; first two columns should be FID and IID')
     parser.add_argument('--INT',
-            type=str,
-            default='adjusted',
-            choices=['', 'marginal', 'adjusted'],
-            help='whether to inverse-normal rank-transform the input data. empty, marginal or adjusted',
-            )
+                        type=str,
+                        default='adjusted',
+                        choices=['', 'marginal', 'adjusted'],
+                        help='whether to inverse-normal rank-transform the input data. empty, marginal or adjusted',
+                        )
     parser.add_argument(
-            '--model',
-            type=str,
-            default='lmmInfOnly',
-            choices=['lmm', 'lmmInfOnly', 'lmmForceNonInf'],
-            help="what lmm model to use in BOLT-LMM. "
-            "`lmm` will run non-inf model only if increase in power expected; "
-            "`lmmInfOnly` will run only inf-model (fast); "
-            " and `lmmForceNonInf` will always run both models",
-            )
+        '--model',
+        type=str,
+        default='lmmInfOnly',
+        choices=['lmm', 'lmmInfOnly', 'lmmForceNonInf'],
+        help="what lmm model to use in BOLT-LMM. "
+        "`lmm` will run non-inf model only if increase in power expected; "
+        "`lmmInfOnly` will run only inf-model (fast); "
+        " and `lmmForceNonInf` will always run both models",
+    )
     parser.add_argument(
-            '--cov_cols',
-            type=str,
-            default=['sex', 'assessment_center', 'geno_batch'],
-            nargs='+',
-            help='categorical covariate columns in covariate file. Defaults to sex, assessment_center and geno_batch',
-            )
+        '--cov_cols',
+        type=str,
+        default=['sex', 'assessment_center', 'geno_batch'],
+        nargs='+',
+        help='categorical covariate columns in covariate file. Defaults to sex, assessment_center and geno_batch',
+    )
     parser.add_argument(
-            '--qcov_cols',
-            type=str,
-            # default=['age', 'genet_PC_{1:10}'],
-            default=[],
-            nargs='+',
-            help='categorical covariate columns in covariate file',
-            )
+        '--qcov_cols',
+        type=str,
+        default=['age', 'genet_PC_{1:41}'],
+        nargs='+',
+        help='quantitative covariate columns in covariate file',
+    )
     parser.add_argument(
-            '--emb',
-            type=str,
-            help='path to space-separated image embedding file; first two columns should be FID and IID, afterwards columns should be named `PC_0`, `PC_1`, ...',
-            )
+        '--emb',
+        type=str,
+        help='path to space-separated image embedding file; first two columns should be FID and IID, afterwards columns should be named `PC_0`, `PC_1`, ...',
+    )
     parser.add_argument(
-            '--first_pc',
-            type=int,
-            default=0,
-            help='will run BOLT-LMM on `first_pc` to `last_pc`. Default: 0',
-            )
+        '--first_pc',
+        type=int,
+        default=0,
+        help='will run BOLT-LMM on `first_pc` to `last_pc`. Default: 0',
+    )
     parser.add_argument(
-            '--last_pc',
-            type=int,
-            default=9,
-            help='will run BOLT-LMM on `first_pc` to `last_pc`. Default: 9',
-            )
+        '--last_pc',
+        type=int,
+        default=9,
+        help='will run BOLT-LMM on `first_pc` to `last_pc`. Default: 9',
+    )
     parser.add_argument('--out_dir', type=str, help='path to output directory')
-    parser.add_argument('--out_fn', type=str, default='PC_%d.txt', help='output filename; should include a `%d` for the PC')
-    parser.add_argument('--bolt', type=str, help='path to BOLT-LMM directory. If none specified, will try to download bolt-lmm to `bolt/`')
-    parser.add_argument('--threads', default=130, type=int, help='how many threads to use for BOLT-LMM')
-    parser.add_argument('--max_missing_snp', default=0.1, type=float, help='maximum missingness per SNP')
-    parser.add_argument('--max_missing_indiv', default=0.1, type=float, help='maximum missingness per individual')
-    parser.add_argument('--ref_map', type=str, help='Alternative genome reference map. If none specified, will use GRCh37/hg19 map from BOLT-LMM')
-    parser.add_argument('--ldscores', type=str, help='LDScores for test calibration. If none specified, will use European ancestry scores from BOLT-LMM')
-
-
-    parser.add_argument('--run_imputed', action='store_true', help='Set flag if you want to run analysis on imputed data')
-    parser.add_argument('--out_fn_imp', default='PC_%d.imp.txt', type=str, help='output filename for imputed; should include a `%d` for the PC')
-    parser.add_argument('--bgen', type=str, help='path to imputed .bgen files. Multiple files via `foo_{1:22}.bgen`')
-    parser.add_argument('--sample', type=str, help='path to imputed .sample files. Multiple files via `foo_{1:22}.sample`')
-    parser.add_argument('--imp_maf', default=0.001, type=float, help='Minor allele frequency in imputed data.')
-    parser.add_argument('--imp_info', default=0.001, type=float, help='Imputation information quality score in imputed data.')
+    parser.add_argument('--out_fn', type=str, default='PC_%d.txt',
+                        help='output filename; should include a `%d` for the PC')
     parser.add_argument(
-            '--remove',
-            type=str,
-            default=[],
-            nargs='+',
-            help='IID to remove. Space-separated file with `FID` and `IID`, one FID/IID-pair per line',
-            )
+        '--bolt', type=str, help='path to BOLT-LMM directory. If none specified, will try to download bolt-lmm to `bolt/`')
+    parser.add_argument('--threads', default=130, type=int,
+                        help='how many threads to use for BOLT-LMM')
+    parser.add_argument('--max_missing_snp', default=0.1,
+                        type=float, help='maximum missingness per SNP')
+    parser.add_argument('--max_missing_indiv', default=0.1,
+                        type=float, help='maximum missingness per individual')
+    parser.add_argument('--ref_map', type=str,
+                        help='Alternative genome reference map. If none specified, will use GRCh37/hg19 map from BOLT-LMM')
+    parser.add_argument('--ldscores', type=str,
+                        help='LDScores for test calibration. If none specified, will use European ancestry scores from BOLT-LMM')
 
-
+    parser.add_argument('--run_imputed', action='store_true',
+                        help='Set flag if you want to run analysis on imputed data')
+    parser.add_argument('--out_fn_imp', default='PC_%d.imp.txt', type=str,
+                        help='output filename for imputed; should include a `%d` for the PC')
+    parser.add_argument(
+        '--bgen', type=str, help='path to imputed .bgen files. Multiple files via `foo_{1:22}.bgen`')
+    parser.add_argument(
+        '--sample', type=str, help='path to imputed .sample files. Multiple files via `foo_{1:22}.sample`')
+    parser.add_argument('--imp_maf', default=0.001, type=float,
+                        help='Minor allele frequency in imputed data.')
+    parser.add_argument('--imp_info', default=0.001, type=float,
+                        help='Imputation information quality score in imputed data.')
+    parser.add_argument(
+        '--remove',
+        type=str,
+        default=[],
+        nargs='+',
+        help='IID to remove. Space-separated file with `FID` and `IID`, one FID/IID-pair per line',
+    )
 
     args = parser.parse_args()
 
-
     if args.config is not None:
         configs = toml.load(args.config)
-        in_transform = configs.pop('INT')   
+        in_transform = configs.pop('INT')
         covars = configs.pop('covColumns')
         qcovars = configs.pop('qCovColumns')
         bolt = check_bolt(configs.pop('boltDirectory'))
@@ -122,7 +134,6 @@ def main():
         first, last = configs.pop('firstPC'), configs.pop('lastPC')
         remove = configs.pop('remove')
 
-
     else:
         in_transform = args.INT
         covars = args.cov_cols
@@ -135,17 +146,17 @@ def main():
         first, last = args.first_pc, args.last_pc
         remove = args.remove
         configs = {
-                'bed': args.bed,
-                'bim': args.bim,
-                'fam': args.fam,
-                'phenoFile': args.emb,
-                'covarFile': args.cov,
-                'maxMissingPerSnp': args.max_missing_snp,
-                'maxMissingPerIndiv': args.max_missing_indiv,
-                'geneticMapFile': args.ref_map,
-                'LDscoresFile': args.ldscores,
-                'numThreads': args.threads,
-                }
+            'bed': args.bed,
+            'bim': args.bim,
+            'fam': args.fam,
+            'phenoFile': args.emb,
+            'covarFile': args.cov,
+            'maxMissingPerSnp': args.max_missing_snp,
+            'maxMissingPerIndiv': args.max_missing_indiv,
+            'geneticMapFile': args.ref_map,
+            'LDscoresFile': args.ldscores,
+            'numThreads': args.threads,
+        }
         flags = [f'--{args.model}']
 
         if run_imputed:
@@ -155,22 +166,24 @@ def main():
             configs['bgenMinINFO'] = args.imp_info
 
         if not args.ref_map:
-            configs['geneticMapFile'] = join(bolt, 'tables', 'genetic_map_hg19_withX.txt.gz')
+            configs['geneticMapFile'] = join(
+                bolt, 'tables', 'genetic_map_hg19_withX.txt.gz')
         if not args.ldscores:
-            configs['LDscoresFile'] = join(bolt, 'tables', 'LDSCORE.1000G_EUR.tab.gz')
+            configs['LDscoresFile'] = join(
+                bolt, 'tables', 'LDSCORE.1000G_EUR.tab.gz')
 
-    
     if in_transform in ['marginal', 'adjusted']:
         dfa = inverse_rank_transform(
-                configs['phenoFile'],
-                cov_fn=configs['covarFile'],
-                covars=covars,
-                qcovars=qcovars,
-                method=in_transform,
-                )
+            configs['phenoFile'],
+            cov_fn=configs['covarFile'],
+            covars=covars,
+            qcovars=qcovars,
+            method=in_transform,
+        )
         tmp_dir = join(out_dir, 'tmp')
         os.makedirs(tmp_dir, exist_ok=True)
-        fn = join(tmp_dir, '.'.join(configs['phenoFile'].split('/')[-1].split('.')[:-1]) + f'_INT_{in_transform}_{uuid.uuid4()}.txt')
+        fn = join(tmp_dir, '.'.join(configs['phenoFile'].split(
+            '/')[-1].split('.')[:-1]) + f'_INT_{in_transform}_{uuid.uuid4()}.txt')
         dfa.to_csv(fn, sep=' ', index=False)
         configs['phenoFile'] = fn
 
@@ -183,8 +196,9 @@ def main():
         log_file = join(out_dir, 'log', out_fn % pc + '.log')
         if run_imputed:
             configs['statsFileBgenSnps'] = join(out_dir, out_imp % pc)
-            
-        run_single_bolt(bolt, flags, covars, qcovars, remove, log_file, **configs)
+
+        run_single_bolt(bolt, flags, covars, qcovars,
+                        remove, log_file, **configs)
 
 
 def inverse_rank_transform(inp_fn, cov_fn=None, covars=None, qcovars=None, method='adjusted'):
@@ -260,7 +274,8 @@ def check_bolt(pth):
         b_tgz = './bolt.tar.gz'
         print('downloading BOLT-LMM ...')
         # TODO: Check for new version
-        request = requests.get('https://storage.googleapis.com/broad-alkesgroup-public/BOLT-LMM/downloads/old/BOLT-LMM_v2.3.4.tar.gz')
+        request = requests.get(
+            'https://storage.googleapis.com/broad-alkesgroup-public/BOLT-LMM/downloads/old/BOLT-LMM_v2.3.4.tar.gz')
         open(b_tgz, 'wb').write(request.content)
         print('finished download')
         tar = tarfile.open(b_tgz, 'r:gz')
@@ -271,12 +286,16 @@ def check_bolt(pth):
 
 
 def run_single_bolt(bolt, flags, covars, qcovars, remove, log_file, **kwargs):
-    config = sum([[f'--{key}', f'{value}'] for key, value in kwargs.items()], [])
-    for c in covars: config += ['--covarCol', c]
-    for q in qcovars: config += ['--qCovarCol', q]
+    config = sum([[f'--{key}', f'{value}']
+                 for key, value in kwargs.items()], [])
+    for c in covars:
+        config += ['--covarCol', c]
+    for q in qcovars:
+        config += ['--qCovarCol', q]
     if isinstance(remove, str) and remove:
         remove = [remove]
-    for r in remove: config += ['--remove', r]
+    for r in remove:
+        config += ['--remove', r]
     config += '--covarMaxLevels 50'.split()
     config += ['--verboseStats']
 
