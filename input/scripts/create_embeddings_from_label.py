fe5f01947f098cbc4b551618795278e994558821
Tim Depping <tim-lorenz@depping.org>
2023-02-24 15:25:58 +0100

    add script to run lmm on label

--- input/scripts/create_embeddings_from_label.py
+++ input/scripts/create_embeddings_from_label.py
@@ -6,19 +6,16 @@ ej_column = '22420-2.0'
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--embeddings_file', type=str,
+    parser.add_argument('-e', '--embeddings_file', type=str,
                         help='Path to file to extract the IDs from')
-    parser.add_argument('--label_file', type=str,
+    parser.add_argument('-l', '--label_file', type=str,
                         help='Path to file to extract the values for the label e.g. ejection fraction')
-    parser.add_argument('--output_file', type=str,
+    parser.add_argument('-o', '--output_file', type=str,
                         help='Path to output file')
     args = parser.parse_args()
 
-    print("here")
     embeddings_df = pd.read_csv(args.embeddings_file, sep=' ')
     label_df = pd.read_csv(args.label_file, sep=',')
-    print(label_df)
-    print(label_df[ej_column])
     merged_df = embeddings_df.merge(label_df, left_on='IID', right_on='eid')
     merged_df = merged_df[['FID', 'IID', ej_column]]
     merged_df.dropna(subset=[ej_column], inplace=True)
