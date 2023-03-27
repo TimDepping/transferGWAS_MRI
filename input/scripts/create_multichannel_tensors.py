d71da84a67f4f03297f01796741052dd9a9b4163
Tim Depping <tim-lorenz@depping.org>
2023-03-27 10:53:41 +0200

    merge create_csv_for_mc_images.py in create_multichannel_tensors.py

--- input/scripts/create_multichannel_tensors.py
+++ input/scripts/create_multichannel_tensors.py
@@ -5,45 +5,64 @@ import numpy as np
 import torch
 from PIL import Image
 from tqdm import tqdm
+import csv
+from pathlib import Path
 
-'''
+"""
 This script creates the multichannel tensors needed for the training_main.py script.
 One multichannel tensor stores all 50 greyscale images of one subject.
-'''
-def store_tensor(tensor, subject_id, out_dir):
-    file_name = join(subject_id + '_CINE_segmented_LAX_4Ch_mc50.pt')
-    file_path = join(out_dir, file_name)
-    torch.save(tensor, file_path)
+"""
 
-def create_tensors(img_dir, out_dir):
-    ## Check if subject contains 50 png images. 
-    subject_ids = tqdm([subject_id for subject_id in os.listdir(img_dir) if len(os.listdir(os.path.join(img_dir, subject_id))) >= 50])
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('img_dir', type=str,
+                        help='path to multichannel input images')
+    parser.add_argument('out_dir', type=str,
+                        help='Directory to save the multichannel tensors')
+    args = parser.parse_args()
+
+    img_dir = args.img_dir
+    out_dir = args.out_dir
+    os.makedirs(out_dir, exist_ok=True)
+
+    subject_ids = tqdm([
+        subject_id
+        for subject_id in os.listdir(img_dir)
+        if len(os.listdir(os.path.join(img_dir, subject_id))) >= 50
+    ])
+
+    img_csv = []
 
     for subject_id in subject_ids:
         subject_path = os.path.join(img_dir, subject_id)
         tensors = []
+
         for i in range(50):
-            file_name = join(subject_id + '_CINE_segmented_LAX_4Ch_' + str(i) + '.png')
+            file_name = f'{subject_id}_CINE_segmented_LAX_4Ch_{i}.png'
             file_path = join(subject_path, file_name)
             img = Image.open(file_path)
             img_tensor = torch.from_numpy(np.array(img))
-            img_tensor = torch.unsqueeze(img_tensor, dim=0) 
+            img_tensor = torch.unsqueeze(img_tensor, dim=0)
             tensors.append(img_tensor)
 
+        # Save stacked tensor to file.
+        output_file_name = f'{subject_id}_CINE_segmented_LAX_4Ch_mc50.pt'
+        output_file_path = join(out_dir, output_file_name)
         stacked_tensor = torch.stack(tensors, dim=0)
+        torch.save(stacked_tensor, output_file_path)
 
-        store_tensor(stacked_tensor, subject_id, out_dir)
+        # Prepare subject to be added to a csv file.
+        img_csv.append({'IID': subject_id, 'path': output_file_name})
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('img_dir', type=str, help='path to multichannel input images')
-    parser.add_argument('out_dir', type=str, help='Directory to save the multichannel tensors')
-    args = parser.parse_args()
+    # Write csv file with all subjects.
+    output_path = Path(args.out_dir)
+    with open(f'{output_path.absolute()}.csv', 'w') as csvfile:
+        fieldnames = ['IID', 'path']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(img_csv)
 
-    # Create base tensors
-    out_dir = args.out_dir
-    os.makedirs(out_dir)
-    create_tensors(args.img_dir, out_dir)
 
 if __name__ == '__main__':
     main()
